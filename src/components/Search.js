93da020eb7aea3cf512acf58593eb5fa5617e82a
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-08 15:35:39 -0500

    edit hooks and async await

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,36 @@
+import axios from "axios";
 import React, {useState, useEffect} from "react";
 
 const Search = () => {
 
-    const [term, setTerm] = useState('');
-    const [termTwo, setTermTwo] = useState('');
+    const [term, setTerm] = useState('Programming');
+    const [results, setResults] = useState([]);
+    
+    
 
     //have to configure useEffect to tell it WHEN we want our code to be executed (via 2nd argument)
     // opt1: empty array, op2: array with term( and / or other array values- will update if ANY of them have changed) inside of it, opt 3: no array at all
     // will ALWAYS see one of these 3 options.
     useEffect( () => {
-        console.log("initial render")
-    })
+        const search = async () => {
+                const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: term
+                    }
+                })
+                setResults(data.query.results);
+                
+                
+            }; 
+                
+                search();
+            
+     
+    }, [term]);
 
     return (
     <div>
