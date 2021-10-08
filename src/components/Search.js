144a76f27330efb045cdc1c77b4deaf0bab25036
MichaelWhitt <michaelwhitt90@gmail.com>
2021-10-08 14:17:41 -0500

    hook update

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,16 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const Search = () => {
 
     const [term, setTerm] = useState('');
+    const [termTwo, setTermTwo] = useState('');
+
+    //have to configure useEffect to tell it WHEN we want our code to be executed (via 2nd argument)
+    // opt1: empty array, op2: array with term( and / or other array values- will update if ANY of them have changed) inside of it, opt 3: no array at all
+    // will ALWAYS see one of these 3 options.
+    useEffect( () => {
+        console.log("initial render")
+    })
 
     return (
     <div>
